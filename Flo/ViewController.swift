1f1312736372a8dd73e7df66ac733b357f8c25d1
Ronald Fischer <rff@ronalds-mbp.attlocal.net>
2015-09-30 22:22:29 -0700

    Adding the custom chart

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isGraphViewShowing = false
+    
     //Counter outlets
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    //Label outlets
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +41,33 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
     }
 
+    @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            
+            //hide Graph
+            UIView.transitionFromView(graphView,
+                toView: counterView,
+                duration: 1.0,
+                options: UIViewAnimationOptions.TransitionFlipFromLeft
+                    | UIViewAnimationOptions.ShowHideTransitionViews,
+                completion:nil)
+        } else {
+            
+            //show Graph
+            UIView.transitionFromView(counterView,
+                toView: graphView,
+                duration: 1.0,
+                options: UIViewAnimationOptions.TransitionFlipFromRight
+                    | UIViewAnimationOptions.ShowHideTransitionViews,
+                completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
 }
 
